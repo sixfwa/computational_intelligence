9efb5d808d846e6f5ff4ee00a2e959181a62f4f9
Francis <farhanawli6@gmail.com>
2019-10-31 16:19:05 +0100

    local search added

--- python_code/algorithms.py
+++ python_code/algorithms.py
@@ -1,12 +1,10 @@
 import time
 import operator
-from graph import CompleteGraph
 
 
 # Parameters CompleteGraph and time limit
 def random_search(graph, limit):
     number_of_tours = graph.number_of_tours()
-    counter = 0
     tours = {}
     start = time.time()
     finish = start + limit
@@ -14,9 +12,7 @@ def random_search(graph, limit):
         tour = graph.random_tour()
         cost = graph.get_tour_cost(tour)
         if not tour in tours:
-            print("Tour: {}\tCost: {}".format(tour, cost))
             tours[tour] = cost
-            counter += 1
         start = time.time()
 
     shortest = min(tours.items(), key=operator.itemgetter(1))
@@ -46,11 +42,35 @@ def two_opt_neighbourhood(tour):
 # Best Neighbour Step Function
 # Takes a neighbourhood (with the same structure as that returned by the two opt function)
 # and returns the shortest tour in the neighbourhood
-def best_neighbour(graph, tour):
+def best_neighbourhood(graph, tour):
     neighbourhood_tours = two_opt_neighbourhood(tour)
     cost_dictionary = {}
     for neighbourhood_tour in neighbourhood_tours:
         cost_dictionary[neighbourhood_tour] = graph.get_tour_cost(tour)
 
     shortest = min(cost_dictionary.items(), key=operator.itemgetter(1))
+    return shortest
+
+# Combining the above algorithm's.
+# After generating a random tour it should repeatedly try to make
+# the best local improving move. When a local optimum is reached,
+# it should generate a new random solution and start the local
+# search procedure from there. This process should continue until
+# the set timer limit expires.
+
+
+def local_search(graph, limit):
+    number_of_tours = graph.number_of_tours()
+    counter = 0
+    tours = {}
+    start = time.time()
+    finish = start + limit
+    while start < finish:
+        random_tour = graph.random_tour()
+        best_tour = best_neighbourhood(graph, random_tour)
+        if not best_tour[0] in tours:
+            tours[best_tour[0]] = best_tour[1]
+        start = time.time()
+
+    shortest = min(tours.items(), key=operator.itemgetter(1))
     return shortest
